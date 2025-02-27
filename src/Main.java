354948279d2ff8561bdd47f31e396d3f5f0d5c32
Dmitriy <dmitriyndubov@gmail.com>
2025-02-27 16:34:16 +0500

    homework "Objects and classes", completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
-
-
     public static void main(String[] args) {
-        Person sarah = new Person("Sarah", 30);
-        System.out.println("sarah.name = " + sarah.getName());
-        System.out.println("sarah.age = " + sarah.getAge());
-        sarah.setAge(31);
-        System.out.println("sarah.getAge() = " + sarah.getAge());
+        Author author1 = new Author("Джордж", "Оруэлл");
+        Author author2 = new Author("Фёдор", "Достоевский");
 
-        Person john = new Person("John", 13);
-        System.out.println("john.name = " + john.getName());
-        System.out.println("john.age = " + john.getAge());
+        Book book1 = new Book("1984", author1, 1949);
+        Book book2 = new Book("Преступление и наказание", author2, 1866);
+
+        System.out.println("Книга: " + book1.getTitle() + ", Автор: " + book1.getAuthor().getFullName() + ", Год издания: " + book1.getPublicationYear());
+        System.out.println("Книга: " + book2.getTitle() + ", Автор: " + book2.getAuthor().getFullName() + ", Год издания: " + book2.getPublicationYear());
+
+
+        book1.setPublicationYear(1950);
+        System.out.println("После изменения года издания:");
+        System.out.println("Книга: " + book1.getTitle() + ", Автор: " + book1.getAuthor().getFullName() + ", Год издания: " + book1.getPublicationYear());
+
     }
 }
